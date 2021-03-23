c792fa419d6b2dc69cfc2623f7986a7eed322856
JCly-rikiu <jcly.rikiu@gmail.com>
2021-03-24 01:17:15 +0800

    finish Moving Work to the GPU

---
+++ Compute Shaders - Rendering One Million Cubes/Assets/Scripts/GPUGraph.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class GPUGraph : MonoBehaviour
+{
+    [SerializeField]
+    ComputeShader computeShader = default;
+
+    [SerializeField, Range(10, 200)]
+    int resolution = 10;
+
+    [SerializeField]
+    FunctionLibrary.FunctionName function = default;
+
+    public enum TransitionMode { Cycle, Random }
+
+    [SerializeField]
+    TransitionMode transitionMode = TransitionMode.Cycle;
+
+    [SerializeField, Min(0f)]
+    float functionDuration = 1f, transitionDuration = 1f;
+
+    float duration;
+
+    bool transitioning;
+    FunctionLibrary.FunctionName transitionFunction;
+
+    ComputeBuffer positionsBuffer;
+
+    static readonly int positionsId = Shader.PropertyToID("_Positions"), resolutionId = Shader.PropertyToID("_Resolution"), stepId = Shader.PropertyToID("_Step"), timeId = Shader.PropertyToID("_Time");
+
+    void OnEnable()
+    {
+        positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
+    }
+
+    void Update()
+    {
+        duration += Time.deltaTime;
+        if (transitioning)
+        {
+            if (duration >= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+
+        }
+        else if (duration >= functionDuration)
+        {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+
+        UpdateFunctionOnGPU();
+    }
+
+    void OnDisable()
+    {
+        positionsBuffer.Release();
+        positionsBuffer = null;
+    }
+
+    void UpdateFunctionOnGPU()
+    {
+        float step = 2f / resolution;
+        computeShader.SetInt(resolutionId, resolution);
+        computeShader.SetFloat(stepId, step);
+        computeShader.SetFloat(timeId, Time.time);
+
+        computeShader.SetBuffer(0, positionsId, positionsBuffer);
+
+        int groups = Mathf.CeilToInt(resolution / 8f);
+        computeShader.Dispatch(0, groups, groups, 1);
+    }
+
+    void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle ? FunctionLibrary.GetNextFunctionName(function) : FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+}
